1b3d4e66d14ef2053b8720a3f7183ebd03c68e71
othmane teffqhi <othmane.teffahi@outlook.com>
2024-01-23 13:37:46 +0100

    -ADD: Validator for Course Entity

---
+++ TrackMyCourseApp/src/01.TrackMyCourse.Server/TrackMyCourseApi/Validations/CourseValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using trackmycourseapi.models;
+
+namespace TrackMyCourseApi.Validations;
+
+public class CourseValidator : AbstractValidator<Course>
+{
+
+    public CourseValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+        RuleFor(x => x.Name).NotNull();
+        RuleFor(x => x.Progress).InclusiveBetween(0, 100);
+        RuleFor(x => x.Description).Length(0, 200);
+        
+    }
+}
---
+++ TrackMyCourseApp/src/01.TrackMyCourse.Server/TrackMyCourseApi/Validations/ValidatorContainer.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using trackmycourseapi.models;
+
+namespace TrackMyCourseApi.Validations;
+
+public static class ValidatorContainer
+{
+    public static void RegisterAppValidatorContainer(this IServiceCollection services)
+    {
+        services.AddScoped<IValidator<Course>, CourseValidator>();
+    }
+}
