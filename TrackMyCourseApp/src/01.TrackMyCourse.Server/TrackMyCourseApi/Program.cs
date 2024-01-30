d2d8f05dc86f5b72c5277d975b789c6094aab649
othmane teffqhi <othmane.teffahi@outlook.com>
2024-01-30 11:57:35 +0100

    -ADD: all missed things in the server side

--- TrackMyCourseApp/src/01.TrackMyCourse.Server/TrackMyCourseApi/Program.cs
+++ TrackMyCourseApp/src/01.TrackMyCourse.Server/TrackMyCourseApi/Program.cs
@@ -1,6 +1,30 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using TrackMyCourseApi.Common.Authentication;
+using TrackMyCourseApi.Common.DateTimeProvider;
+using TrackMyCourseApi.Data;
+using TrackMyCourseApi.Endpoints;
+using TrackMyCourseApi.Repositories.Interfaces;
+using TrackMyCourseApi.Repositories.RepositoryBase;
+using TrackMyCourseApi.Services.DateTimeProvider;
+using TrackMyCourseApi.Validations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("TrackMyCourseDb"));
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+//Add option Pattern
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection(JwtSettings.SETTINGS));
+
+//Add Validation
+builder.Services.RegisterAppValidatorContainer();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,29 +40,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+//Map the endpoints
+app.MapCourseEndpoints();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+//Seed the data
+SeedData.PrepData(app);
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
